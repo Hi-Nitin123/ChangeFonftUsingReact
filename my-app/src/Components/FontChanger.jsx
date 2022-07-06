311a2e299093240d17d41a32b6e302f2b879d423
Nitin Pandey <npandey@deqode.com>
2022-07-06 18:04:30 +0530

    Create a component FontChange and add text area into it

---
+++ my-app/src/Components/FontChanger.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function FontChanger() {
+  return (
+    <div>
+      <label htmlFor="">
+        <textarea
+          name=""
+          id=""
+          cols="100"
+          rows="10"
+          placeholder="Write something..."
+        ></textarea>
+      </label>
+    </div>
+  );
+}
+
+export default FontChanger;
