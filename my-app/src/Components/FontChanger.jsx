7b4d7da4f908a0a62b7e49b5aa60e8af7454b621
Nitin Pandey <npandey@deqode.com>
2022-07-06 18:18:11 +0530

    Add three buttons in FontChanger comonent

--- my-app/src/Components/FontChanger.jsx
+++ my-app/src/Components/FontChanger.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
+import "../css/FontChanger.css";
+
 function FontChanger() {
   return (
     <div>
       <label htmlFor="">
         <textarea
           name=""
-          id=""
+          id="txt"
           cols="100"
           rows="10"
           placeholder="Write something..."
         ></textarea>
+        <br />
+        <br />
       </label>
+      <div>
+        <button id="btn1">Bold</button>
+        <button id="btn2">Italic</button>
+        <button id="btn3">General</button>
+      </div>
     </div>
   );
 }
