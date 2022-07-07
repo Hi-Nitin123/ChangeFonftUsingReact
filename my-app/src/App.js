85eea10d97abf297b1e1d8aaed15670dcca2d35c
Nitin Pandey <npandey@deqode.com>
2022-07-07 16:27:59 +0530

    Put some css to the components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import FontChanger from "./Components/FontChanger";
+import React, { useState } from "react";
+
+export const Data = React.createContext();
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <FontChanger />
+      <Data.Provider value={[input, setInput]}>
+        <FontChanger />
+      </Data.Provider>
     </div>
   );
 }
--- my-app/src/Components/FontChanger.jsx
+++ my-app/src/Components/FontChanger.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../css/FontChanger.css";
+import Bold from "./Bold";
+import General from "./General";
+import Italic from "./Italic";
 
 function FontChanger() {
+  const [general, setGeneral] = useState(true);
+  const [bold, setBold] = useState(false);
+  const [italic, setItalic] = useState(false);
+  console.log(italic);
+
+  const makeItBold = () => {
+    setBold(true);
+    setItalic(false);
+    setGeneral(false);
+  };
+
+  const makeItItalic = () => {
+    setBold(false);
+    setItalic(true);
+    setGeneral(false);
+  };
+
+  const makeItGeneral = () => {
+    setBold(false);
+    setItalic(false);
+    setGeneral(true);
+  };
+
   return (
     <div>
-      <label htmlFor="">
-        <textarea
-          name=""
-          id="txt"
-          cols="100"
-          rows="10"
-          placeholder="Write something..."
-        ></textarea>
-        <br />
-        <br />
-      </label>
-      <div>
-        <button id="btn1">Bold</button>
-        <button id="btn2">Italic</button>
-        <button id="btn3">General</button>
-      </div>
+      {general ? <General /> : bold ? <Bold /> : <Italic />}
+
+      <button id="btn1" onClick={makeItBold}>
+        Bold
+      </button>
+      <button id="btn2" onClick={makeItItalic}>
+        Italic
+      </button>
+      <button id="btn3" onClick={makeItGeneral}>
+        General
+      </button>
     </div>
   );
 }
